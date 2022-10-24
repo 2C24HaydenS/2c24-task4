9e96924ac46f1894cd37ac94d467c1d02ce520c2
2C24HaydenS <115450543+2C24HaydenS@users.noreply.github.com>
2022-10-24 14:59:18 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,22 @@
-let y_value = 0
 let x_value = 0
+let y_value = 0
 input.onButtonPressed(Button.A, function () {
-	
+    x_value = 0
+    while (x_value <= 4) {
+        led.plot(x_value, 0)
+        basic.pause(500)
+        basic.clearScreen()
+        x_value += 1
+    }
 })
 input.onButtonPressed(Button.B, function () {
-	
+    y_value = 0
+    while (y_value <= 4) {
+        led.plot(0, y_value)
+        basic.pause(500)
+        basic.clearScreen()
+        y_value += 1
+    }
 })
 basic.forever(function () {
     if (input.buttonIsPressed(Button.B)) {
